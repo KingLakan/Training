2e653b3de22157469ca597f65c36488dc51108f7
KingLakan <32309095+KingLakan@users.noreply.github.com>
2022-04-02 21:03:35 +0200

    Update Append.py

--- src/Append.py
+++ src/Append.py
@@ -1,5 +1,7 @@
-with open("../dummy.txt", 'w') as file_object:
-    file_object.write("hello\n")
+# with open("../dummy.txt", 'w') as file_object:
+#    file_object.write("hello\n")
 with open("../dummy.txt", 'a') as file_object:
-    file_object.write("hello\n")
+    file_object.write("röv\n")
+    file_object.write("röv\n")
+    file_object.write("röv\n")
 print("append")
