7909f2e0115733fa8b2301c5b27e7d5c9a148d96
David Samuelsson <david.pb.sammuelsson@gmail.com>
2022-04-03 20:42:05 +0200

    Append argpars

--- Append.py
+++ Append.py
@@ -1,2 +1,15 @@
-with open("dummy.txt", 'a') as file_object:
+import argparse
+
+parser = argparse.ArgumentParser(description='SysDef to be processed')
+parser.add_argument('--file',
+                    metavar='FILE_PATH',
+                    type=str,
+                    action='store',
+                    help='Path+name of file to append to')
+
+args = parser.parse_args()
+print(args, "\n")
+print("inputted file: {}".format(args.file))
+
+with open(args.file, 'a') as file_object:
     file_object.write("hello\n")
