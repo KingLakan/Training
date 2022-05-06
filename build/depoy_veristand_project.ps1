32dd9e4632ed1b1e7ffefa559c06f68dda78b943
KingLakan <32309095+KingLakan@users.noreply.github.com>
2022-05-06 11:45:10 +0200

    Update depoy_veristand_project.ps1

--- build/depoy_veristand_project.ps1
+++ build/depoy_veristand_project.ps1
@@ -2,7 +2,7 @@
 
 Write-Host "Running deploy veristand"
 #$test = start-Process -FilePath "C:\Program Files\National Instruments\VeriStand 2020\veristand.exe" -ArgumentList '-openProject "C:\Users\dsamuels\Documents\VeriStand Projects\Engine Demo 2\Engine Demo 2.nivsprj"','-sysDef "C:\Users\dsamuels\Documents\VeriStand Projects\Engine Demo 2\Engine Demo.nivssdf"', '-deploy' -passThru -Wait
-$test = start-Process -FilePath "C:\Program Files\National Instruments\VeriStand 2020\veristand.exe" -ArgumentList '-openProject "C:\Users\dsamuels\Documents\VeriStand Projects\Engine Demo 2\Engine Demo 2.nivsprj"','-sysDef "C:\Users\dsamuels\Documents\VeriStand Projects\Engine Demo 2\Engine Demo.nivssdf"' -passThru -RedirectStandardOutput Out.txt -RedirectStandardError Err.txt -Wait
+$test = start-Process -FilePath "C:\Program Files\National Instruments\VeriStand 2020\veristand.exe" -ArgumentList '-openProject "C:\Users\dsamuels\Documents\VeriStand Projects\Engine Demo 2\Engine Demo 2.nivsprj"','-sysDef "C:\Users\dsamuels\Documents\VeriStand Projects\Engine Demo 2\Engine Demo.nivssdf"' -passThru -RedirectStandardOutput Out.txt -RedirectStandardError Err.txt
 Write-Host "Done"
 exit 0
 #$test.StartTime 
